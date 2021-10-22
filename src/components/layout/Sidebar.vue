c9161c78e408b8a2b8100c2ca73ae6f94c24b27a
monethip <monethip1299@gmail.com>
2021-10-22 13:52:39 +0700

    sidebar

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -11,34 +11,10 @@
       <v-icon style="padding: 4px" @click="drawer = !drawer"
         >mdi-order-bool-descending
       </v-icon>
-      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" > mdi-order-bool-descending</v-app-bar-nav-icon> -->
       <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
-      <!-- <div>
-      <v-text-field
-        rounded
-        dense
-        outlined
-        :prepend-inner-icon="mdi - magnify"
-        class="app-bar-search flex-grow-0"
-        hide-details
-      ></v-text-field>
-
-      <v-spacer />
-      <v-fade-transition mode="out-in">
-        <v-icon
-          :key="$vuetify.theme.dark"
-          @click="$vuetify.theme.dark = !$vuetify.theme.dark"
-        >
-          {{ $vuetify.theme.dark ? mdi - github : mdi - bell - outline }}
-        </v-icon>
-      </v-fade-transition>
-      <v-btn icon small class="ms-3">
-        <v-icon class="ms-6 me-4"> mdi-bell-outline </v-icon>
-      </v-btn>
-     </div> -->
       <v-spacer></v-spacer>
       <v-menu bottom min-width="200px" rounded offset-y>
         <template v-slot:activator="{ on }">
@@ -107,9 +83,6 @@
       </v-list>
     </v-navigation-drawer>
 
-    <!-- <v-main>
-      <router-view />
-    </v-main> -->
   </div>
 </template>
 
