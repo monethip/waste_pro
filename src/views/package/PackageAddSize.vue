c0657270d7bc7f0a37911fbd1892fa8613588df4
xone-sps <xone.sps@gmail.com>
2021-11-23 16:47:31 +0700

    update plan

--- src/views/package/PackageAddSize.vue
+++ src/views/package/PackageAddSize.vue
@@ -21,25 +21,28 @@ export default {
   methods: {
     fetchData() {
       this.$store.commit("Loading_State", true);
-      this.$axios.get("package").then((res) => {
-        if (res.data.code == 200) {
-          setTimeout(() => {
-            this.$store.commit("Loading_State", false);
-            this.packages = res.data.data;
-            this.pagination = res.data.data.pagination;
-            console.log("data", this.packages);
-          },300);
-        }
-      }).catch((error)=>{
+      this.$axios
+        .get("package")
+        .then((res) => {
+          if (res.data.code == 200) {
+            setTimeout(() => {
+              this.$store.commit("Loading_State", false);
+              this.packages = res.data.data;
+              this.pagination = res.data.data.pagination;
+              console.log("data", this.packages);
+            }, 300);
+          }
+        })
+        .catch((error) => {
           this.$store.commit("Loading_State", false);
           this.fetchData();
-          if(error.respones.status == 422){
-              var obj = error.respones.data.errors;
-              for(let [key, message] of Object.entries(obj)){
-                  this.server_errors[key] = message[0]
-              }
+          if (error.respones.status == 422) {
+            var obj = error.respones.data.errors;
+            for (let [key, message] of Object.entries(obj)) {
+              this.server_errors[key] = message[0];
+            }
           }
-      })
+        });
     },
   },
 
@@ -50,4 +53,4 @@ export default {
 </script>
  
  <style>
-</style>
+</style> 
