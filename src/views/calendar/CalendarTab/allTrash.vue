de2722e4355be991554d2073eceb6bbe0134181f
leenaa46 <hsherlock22@gmail.com>
2022-09-05 22:35:41 +0700

    Fix Add round

--- src/views/calendar/CalendarTab/allTrash.vue
+++ src/views/calendar/CalendarTab/allTrash.vue
@@ -19,75 +19,76 @@
       <!--      </v-col>-->
     </v-row>
     <div>
-      <v-data-table v-if="calendars" :headers="headers" :items="calendars" :search="search" :disable-pagination="true"
-        hide-default-footer>
-        <template v-slot:item.customer="{ item }">
-          <div v-if="(item.route_plan_detail.customer.customer_type = 'company')">
-            {{ item.route_plan_detail.customer.company_name }}
-          </div>
-          <div>
-            {{ item.route_plan_detail.customer.name }}
-            {{ item.route_plan_detail.customer.surname }}
-          </div>
-        </template>
-        <template v-slot:item.created_at="{ item }">
-          <div>
-            {{ moment(item.created_at).format("DD-MM-YY hh:mm ") }}
-          </div>
-        </template>
-        <template v-slot:item.date="{ item }">
-          <div>
-            {{ moment(item.date).format("DD-MM-YY hh:mm:ss") }}
-          </div>
-        </template>
-        <template v-slot:item.status="{ item }">
-          <v-chip label :color="statusColor(item.status)">{{ item.status }}</v-chip>
-        </template>
-        <template v-slot:item.amount="{ item }">
-          <div v-if="item.collection_type == 'bag' || item.collection_type == 'chartered'">
-            <v-chip color="primary">{{ item.bag }}</v-chip>
-            <span> {{ getUnit(item.collection_type) }}</span>
-          </div>
-          <div v-else-if="item.collection_type == 'fix_cost'">
-            <span> {{ getUnit(item.collection_type) }}</span>
-          </div>
-          <div v-else>
-            <v-chip color="success">{{ item.container }}</v-chip>
-            <span> {{ getUnit(item.collection_type) }}</span>
-          </div>
-        </template>
+      <v-dialog v-model="dialog" scrollable max-width="300px">
 
-        <template v-slot:item.actions="{ item }">
-          <v-icon small class="mr-2" @click="viewPage(item.plan_calendar_id, item.id)">
-            mdi-eye
-          </v-icon>
+        <template v-slot:activator="{ on, attrs }">
+          <v-data-table v-if="calendars" :headers="headers" :items="calendars" :search="search"
+            :disable-pagination="true" hide-default-footer>
+            <template v-slot:item.customer="{ item }">
+              <div v-if="(item.route_plan_detail.customer.customer_type = 'company')">
+                {{ item.route_plan_detail.customer.company_name }}
+              </div>
+              <div>
+                {{ item.route_plan_detail.customer.name }}
+                {{ item.route_plan_detail.customer.surname }}
+              </div>
+            </template>
+            <template v-slot:item.created_at="{ item }">
+              <div>
+                {{ moment(item.created_at).format("DD-MM-YY hh:mm ") }}
+              </div>
+            </template>
+            <template v-slot:item.date="{ item }">
+              <div>
+                {{ moment(item.date).format("DD-MM-YY hh:mm:ss") }}
+              </div>
+            </template>
+            <template v-slot:item.status="{ item }">
+              <v-chip label :color="statusColor(item.status)">{{ item.status }}</v-chip>
+            </template>
+            <template v-slot:item.amount="{ item }">
+              <div v-if="item.collection_type == 'bag' || item.collection_type == 'chartered'">
+                <v-chip color="primary">{{ item.bag }}</v-chip>
+                <span> {{ getUnit(item.collection_type) }}</span>
+              </div>
+              <div v-else-if="item.collection_type == 'fix_cost'">
+                <span> {{ getUnit(item.collection_type) }}</span>
+              </div>
+              <div v-else>
+                <v-chip color="success">{{ item.container }}</v-chip>
+                <span> {{ getUnit(item.collection_type) }}</span>
+              </div>
+            </template>
 
-          <v-dialog v-model="dialog" scrollable max-width="300px">
-            <template v-slot:activator="{ on, attrs }">
-              <v-icon small class="mr-2" v-bind="attrs" v-on="on">
+            <template v-slot:item.actions="{ item }">
+              <v-icon small class="mr-2" @click="viewPage(item.plan_calendar_id, item.id)">
+                mdi-eye
+              </v-icon>
+              <v-icon small class="mr-2" v-bind="attrs" v-on="on" @click="selectedRound = item.id">
                 mdi-plus
               </v-icon>
             </template>
-            <v-card>
-              <v-card-title>ເພີ່ມຈຳນວນຮອບໃນມື້</v-card-title>
-              <v-divider></v-divider>
-              <v-card-text>
-                <v-text-field type="number" v-model="round"></v-text-field>
-              </v-card-text>
-              <v-divider></v-divider>
-              <v-card-actions>
-                <v-btn color="blue darken-1" text @click="cancelDialog()">
-                  Close
-                </v-btn>
-                <v-btn color="blue darken-1" text @click="addRoundDialog(item)">
-                  Save
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
+          </v-data-table>
 
         </template>
-      </v-data-table><br />
+        <v-card>
+          <v-card-title>ເພີ່ມຈຳນວນຮອບໃນມື້</v-card-title>
+          <v-divider></v-divider>
+          <v-card-text>
+            <v-text-field type="number" v-model="round"></v-text-field>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="blue darken-1" text @click="cancelDialog()">
+              Close
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="addRoundDialog()">
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <br />
       <template>
         <Pagination v-if="pagination.total_pages > 1" :pagination="pagination" :offset="offset" @paginate="fetchData()">
         </Pagination>
@@ -147,7 +148,10 @@ export default {
           sortable: false,
         },
         { text: "", value: "actions", sortable: false },
+        { text: "", value: "add_round", sortable: false },
       ],
+
+      selectedRound: "",
     };
   },
   methods: {
@@ -157,11 +161,11 @@ export default {
     cancelDialog() {
       this.dialog = false;
     },
-    addRoundDialog(item) {
+    addRoundDialog() {
       if (this.round > 0) {
         this.$store.commit("Loading_State", true);
         this.$axios
-          .post("plan-calendar-detail-round/" + item.id, {
+          .post("plan-calendar-detail-round/" + this.selectedRound, {
             round: this.round
           })
           .then((res) => {
@@ -170,6 +174,7 @@ export default {
                 this.$store.commit("Loading_State", false);
                 this.round = 0;
                 this.dialog = false;
+                this.selectedRound = ""
                 this.fetchData()
 
               }, 100);
@@ -179,6 +184,7 @@ export default {
             this.loading = false;
             this.round = 0;
             this.dialog = false;
+            this.selectedRound = ""
             this.fetchData();
             this.$store.commit("Toast_State", {
               value: true,
