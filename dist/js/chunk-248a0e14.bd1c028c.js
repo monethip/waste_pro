4e025b1e52f45f922554db449852817af7a4d12a
leenaa46 <hsherlock22@gmail.com>
2023-11-07 11:09:31 +0700

    Build

---
+++ dist/js/chunk-248a0e14.bd1c028c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-248a0e14"],{2012:function(e,t,n){e.exports=n.p+"img/NewBg.ace44948.jpg"},eec5:function(e,t,n){"use strict";n.r(t);var r=n("2c6a"),c=function(){var e=this,t=e._self._c;return t(r["a"],{attrs:{src:n("2012"),"backgound-size":"cover",width:"120rem",height:"61rem",content:"center"}})},u=[],a={data(){return{}},computed:{time(){return(new Date).toISOString().substr(0,15)}}},o=a,s=n("d22c"),i=Object(s["a"])(o,c,u,!1,null,null,null);t["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-248a0e14.bd1c028c.js.map
